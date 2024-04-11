fed5ba9f4aed2304df42c8e62d0d0b7021de7568
Teta Iris Credot <tetairiscredot@gmail.com>
2024-04-11 10:31:48 +0200

    Updated the configs

--- configs/index.js
+++ configs/index.js
@@ -1,7 +1,7 @@
 const configs = {
     port: process.env.PORT || 3000,
     mongoURI: process.env.MONGODB_URI || 'mongodb+srv://iris-credot:Niwenshuti250@cluster0.rp1pdv9.mongodb.net/contact-app?retryWrites=true&w=majority&appName=Cluster0',
-    secret: process.env.SECRET ||'mysecret'
+    
 }
 
 export default configs;
